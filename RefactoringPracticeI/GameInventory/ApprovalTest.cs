507bb03ba7426a68f3a35c414c801af2da8423e7
rodrigocamperopaz <rodrigo.campero@trueforce.com>
2022-01-05 09:18:51 -0400

    Initial Commit

---
+++ RefactoringPracticeI/GameInventory/ApprovalTest.cs
@@ -0,0 +1,26 @@
+﻿using ApprovalTests;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+using System.Text;
+
+namespace RefactoringExerciseI
+{
+    [UseReporter(typeof(DiffReporter))]
+    [TestFixture]
+    public class ApprovalTest
+    {
+        [Test]
+        public void ThirtyDays()
+        {
+
+            StringBuilder fakeoutput = new StringBuilder();
+            Console.SetOut(new StringWriter(fakeoutput));
+            Console.SetIn(new StringReader("a\n"));
+
+            Program.Main(new string[] { });
+            var output = fakeoutput.ToString();
+
+            Approvals.Verify(output);
+        }
+    }
+}
---
+++ RefactoringPracticeI/GameInventory/ConcertTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace RefactoringExerciseI
+{
+    [TestFixture]
+    public class GameInventoryTest
+    {
+        [Test]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GameInventory app = new GameInventory(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("fixme", Items[0].Name);
+        }
+    }
+}
